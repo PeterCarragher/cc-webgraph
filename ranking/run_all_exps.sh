534e7dad98c58cef471df05a855d276630de67b2
Peter Carragher <petercarragher6@gmail.com>
2024-02-12 17:17:52 -0500

    exps

--- ranking/run_all_exps.sh
+++ ranking/run_all_exps.sh
@@ -5,9 +5,13 @@ LABELS=../data/attributes.csv # labelled domains that we care about final ranks 
 VERTICES=./ranking/data/cc-main-2023-may-sep-nov-domain-vertices.txt # the graph that PPR runs on
 
 
+./ranking/run_ppr.sh $LABELS $VERTICES - exp-baseline 0
+
+./ranking/run_ppr.sh $LABELS $VERTICES \
+    ./ranking/data/preference_vectors/domain_lists/rel_domains.txt exp-rel-ppr 0
+
+./ranking/run_ppr.sh $LABELS $VERTICES \
+    ./ranking/data/preference_vectors/domain_lists/unrel_domains.txt exp-unrel-ppr-down 1
+
 ./ranking/run_ppr.sh $LABELS $VERTICES \
     ./ranking/data/preference_vectors/domain_lists/link_scheme_domains.txt exp-ls-ppr-down 1
-
-
-# ./run_ppr.sh $LABELS $VERTICES \
-#     ../data/preference_vectors/domain_lists/link_scheme_domains.txt exp-ls-ppr-down 1
