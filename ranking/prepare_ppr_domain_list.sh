3f3e43c1b11e3e811f2dab9b50f0eb1a0f7d956e
Peter Carragher <petercarragher6@gmail.com>
2024-02-15 15:37:00 -0500

    link scheme list from webgraph

--- ranking/prepare_ppr_domain_list.sh
+++ ranking/prepare_ppr_domain_list.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -x
+
 cd "$(dirname "$(dirname "$(realpath "$0")")")"
 
 reverse_urls() {
@@ -28,14 +31,61 @@ reverse_urls() {
 
 LABELS=../data/attributes.csv
 REL_LIST=./ranking/data/preference_vectors/domain_lists/rel_domains.txt
-awk '$2 >= 5 { print $1 }' FS=,  $LABEL > $REL_LIST
+awk '$2 >= 5 { print $1 }' FS=,  $LABELS > $REL_LIST
 reverse_urls $REL_LIST
 
 UNREL_LIST=./ranking/data/preference_vectors/domain_lists/unrel_domains.txt
-awk '$2 < 5 { print $1 }' FS=,  $LABEL > $UNREL_LIST
+awk '$2 < 5 { print $1 }' FS=,  $LABELS > $UNREL_LIST
 reverse_urls $UNREL_LIST
 
-# LS_LIST=./ranking/data/preference_vectors/domain_lists/link_scheme_domains.txt
+find_link_schemes() {
+    local id_list=$1
+    local edge_list=$2
+    local output_file=$3
+
+    # Count occurrences of source IDs linking to domains in the vertex ID list
+    awk -F'\t' 'NR==FNR { 
+            ids[$1] = 1
+            next
+        } 
+        {
+            if ($1 in ids) {
+                source_count[$1]++
+            }
+        } 
+        END {
+            for (source_id in source_count) {
+                if (source_count[source_id] >= 4) {
+                    print source_id $source_count[source_id]
+                }
+            }
+        }' "$id_list" "$edge_list" > "$output_file"
+}
+
+fetch_vertex_ids() {
+    local domain_list=$1
+    local id_list=$2
+    local output_file=$3
+    awk 'NR==FNR { 
+            urls[$1] = 1
+            next 
+        } 
+        {
+            if ($2 in urls) {
+                print $1;
+            }
+        }' FS=, "$domain_list" FS='\t' "$id_list" > "$output_file"
+    sort -n "$output_file" -o "$output_file"
+}
+
+EDGES=./ranking/data/cc-main-2023-may-sep-nov-domain-edges.txt
+VERTICES=./ranking/data/cc-main-2023-may-sep-nov-domain-vertices.txt
+fetch_vertex_ids $UNREL_LIST $VERTICES $UNREL_LIST.ids
+
+LS_LIST=./ranking/data/preference_vectors/domain_lists/cc_link_scheme_domains.txt
+find_link_schemes $UNREL_LIST.ids $EDGES $LS_LIST
+sort -n $LS_LIST -o $LS_LIST
+
 # conda activate lsr
 # cd ../interventions/
 # python3 link_schemes.py > $LS_LIST
